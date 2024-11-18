191f7425e103a9d0e69f5cf6a0563c922a07e28d
zzhhjjj <z785566960@gmail.com>
2024-11-18 17:36:51 +0000

    add mfu, get number of parameters

--- picotron/utils.py
+++ picotron/utils.py
@@ -5,6 +5,7 @@ import numpy as np
 import builtins
 import fcntl
 import picotron.process_group_manager as pgm
+import torch, torch.distributed as dist
 
 def print(*args, is_print_rank=True, **kwargs):
     """ solves multi-process interleaved print problem """
@@ -32,6 +33,47 @@ def to_readable_format(num, precision=2):
         return f"{num / 1e3:.{precision}f}K"
     else:
         return f"{num:.{precision}f}"
+
+# ref: https://github.com/karpathy/nanoGPT/blob/9755682b981a45507f6eb9b11eadef8cb83cebd5/model.py#L289
+def get_mfu(tokens_per_second, num_params, model_config, theoretical_flops = 989 * 10 ** 12):
+    num_layers = model_config.num_hidden_layers
+    hidden_dim = model_config.hidden_size
+    seq_len = model_config.max_position_embeddings
+    flops_per_toke = 6 * num_params + 12 * num_layers * hidden_dim * seq_len
+    mfu = tokens_per_second * flops_per_toke / theoretical_flops * 100 # percentage
+    return mfu
+
+def get_num_params(model):
+    """Calculate total number of parameters accounting for tensor parallelism and pipeline parallelism.
+    
+    For TP: Parameters in attention/mlp/embed/final_proj are sharded, so multiply by tp_world_size
+    For PP: Need to gather parameter counts across pipeline stages
+    For DP: Parameters are replicated, so only count once
+    
+    Note: 
+    LayerNorm: Split across TP ranks for sequence parallelism
+    FSDP: Parameters are sharded across data parallel ranks
+    """
+    tp_world_size = pgm.process_group_manager.tp_world_size
+    
+    # Count parameters in current PP rank
+    local_num_params = 0
+    for name, param in model.named_parameters():
+        # Parameters split across TP ranks
+        # TODO: LayerNorm is also split across TP ranks for sequence parallelism
+        if any(tp_keyword in name.lower() for tp_keyword in ['attention', 'mlp', 'embed', 'final_proj']):
+            local_num_params += param.numel() * tp_world_size
+        else:
+            # Parameters replicated across TP ranks (layer norm, biases)
+            local_num_params += param.numel()
+            
+    # Gather parameter counts from all PP ranks
+    param_counts = torch.tensor(local_num_params, device='cuda')
+    
+    # Sum up parameters across all PP ranks
+    dist.all_reduce(param_counts, op=dist.ReduceOp.SUM, group=pgm.process_group_manager.pp_group)
+    
+    return param_counts.item()
 
 def save_checkpoint(model, optimizer, trained_steps, trained_tokens, out_dir):
     """Save the model/optimizer states/steps to a checkpoint file."""
